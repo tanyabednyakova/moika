f3e7cae8c8ccbf3daf884ea6ea6bb3d529250223
Blajimir <blajimir@gmail.com>
2017-02-08 19:40:14 +0300

    Незначительные изменения - Рефакторинг ;)

--- src/main/java/io/khasang/moika/controller/AppController.java
+++ src/main/java/io/khasang/moika/controller/AppController.java
@@ -2,7 +2,6 @@ package io.khasang.moika.controller;
 
 import io.khasang.moika.sometest.SomeTest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,7 +14,6 @@ public class AppController {
     public String hello(Model model) {
         model.addAttribute("hello", "Hello spring!!!");
         model.addAttribute("someStr",someTest.getStr());
-
         return "index";
     }
 }
