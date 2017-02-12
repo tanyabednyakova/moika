f35bb45061d105b283b813355f387a684c5eff78
tanyabednyakova <tmbednyakova@gmail.com>
2017-02-12 11:40:49 +0300

    testBD

--- src/main/java/io/khasang/moika/config/application/WebConfig.java
+++ src/main/java/io/khasang/moika/config/application/WebConfig.java
@@ -11,7 +11,7 @@ import org.springframework.web.servlet.view.JstlView;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan({"io.khasang.moika.controller"})
+@ComponentScan({"io.khasang.moika.controller","io.khasang.moika.config", "io.khasang.moika.model"})
 public class WebConfig extends WebMvcConfigurerAdapter {
     @Bean
     public InternalResourceViewResolver viewResolver() {
--- src/main/java/io/khasang/moika/controller/AppController.java
+++ src/main/java/io/khasang/moika/controller/AppController.java
@@ -1,12 +1,29 @@
 package io.khasang.moika.controller;
 
+import io.khasang.moika.model.CreateTable;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+
 
 @Controller
 public class AppController {
+
+    @Autowired
+    CreateTable createTable;
+
     @RequestMapping("/")
     public String hello() {
+
         return "index";
+    }
+
+
+    @RequestMapping("/test")
+    public String test(Model model) {
+        model.addAttribute("test", createTable.create());
+        return "testout";
     }
 }
