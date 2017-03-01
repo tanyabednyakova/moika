6c95c475bc84298f8ea572c882917fc8a9701aac
aalyubarev <lyuks@bk.ru>
2017-03-01 15:30:52 +0300

    add CarDAO

---
+++ src/main/java/io/khasang/moika/dao/CarDAO.java
@@ -0,0 +1,7 @@
+package io.khasang.moika.dao;
+
+/**
+ * Created by alyubarev on 01.03.2017.
+ */
+public interface CarDAO {
+}
---
+++ src/main/java/io/khasang/moika/dao/impl/CarDAOImpl.java
@@ -0,0 +1,7 @@
+package io.khasang.moika.dao.impl;
+
+/**
+ * Created by alyubarev on 01.03.2017.
+ */
+public class CarDAOImpl {
+}
