cf8f1ab5a9b1b7ec1428a241a4576be6912fe12c
vol2hv <vol2hv@yandex.ru>
2017-03-03 20:47:45 +0300

    Добавил WorkDAO

---
+++ src/main/java/io/khasang/moika/dao/WorkDAO.java
@@ -0,0 +1,16 @@
+package io.khasang.moika.dao;
+
+import io.khasang.moika.entity.Work;
+
+import java.util.List;
+
+public interface WorkDAO {
+    public interface WashFacilityDao {
+        void addWork(Work work);
+        void updateWork(Work work);
+        void deleteWork(Work work);
+        Work getWork(long id);
+        Work getWork(String name );
+        List<Work> getAllWork();
+     }
+}
