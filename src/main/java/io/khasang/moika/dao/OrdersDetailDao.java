c1497d5f31463cca5c5837f4d08d346c0b4f3e04
vol2hv <vol2hv@yandex.ru>
2017-03-04 18:18:04 +0300

    промежуточный

--- src/main/java/io/khasang/moika/dao/OrdersDetailDao.java
+++ src/main/java/io/khasang/moika/dao/OrdersDetailDao.java
@@ -1,4 +1,15 @@
 package io.khasang.moika.dao;
 
+import io.khasang.moika.entity.OrdersDetail;
+import io.khasang.moika.entity.Work;
+
+import java.util.List;
+
 public interface OrdersDetailDao {
+    void addOrdersDetail(OrdersDetail ordersDetail);
+    void updatOrdersDetail(OrdersDetail ordersDetail);
+    void deleteOrdersDetailk(OrdersDetail ordersDetail);
+    Work getOrdersDetail(long id);
+    Work getOrdersDetail(String name);
+    List<OrdersDetail> getAllWork();
 }
