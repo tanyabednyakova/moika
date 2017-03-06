0b211e8e62c8cd599f5db70124a8a572061d1f0c
blajimir <blajimir@gmail.com>
2017-03-06 18:32:32 +0300

    доработка js-скриптов и шаблона сайта (reverted from commit c4f1ccbfda97b7ef7b039c638cb79d148dfae24e)

--- src/main/java/io/khasang/moika/service/impl/UserServiceImpl.java
+++ src/main/java/io/khasang/moika/service/impl/UserServiceImpl.java
@@ -1,31 +1,18 @@
 package io.khasang.moika.service.impl;
 
-import io.khasang.moika.dao.RoleDAO;
-import io.khasang.moika.dao.UserDAO;
-import io.khasang.moika.entity.Role;
 import io.khasang.moika.entity.User;
 import io.khasang.moika.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.util.StringUtils;
-import org.springframework.validation.BindingResult;
-
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
 
 public class UserServiceImpl implements UserService {
+
     @Autowired
     private PasswordEncoder passwordEncoder;
-    @Autowired
-    private UserDAO userDAO;
-    @Autowired
-    private RoleDAO roleDAO;
 
     @Override
-    public void createUser(User user) {
-
+    public User createUser(User user) {
+        return null;
     }
 
     @Override
@@ -34,8 +21,9 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    public void updateUser(User user) {
+    public User updateUser(User user) {
         //TODO
+        return null;
     }
 
     @Override
