6f9b832dca160824bd8dd01ecbcfc9b3a206cb93
vol2hv <vol2hv@yandex.ru>
2017-03-11 11:55:19 +0300

    промежуточный 1

---
+++ src/main/java/io/khasang/moika/controller/OrdermController.java
@@ -0,0 +1,27 @@
+package io.khasang.moika.controller;
+
+import io.khasang.moika.entity.Orderm;
+import io.khasang.moika.service.OrdermAccessService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+
+
+@Controller
+@RequestMapping("/orderm")
+public class OrdermController {
+    @Autowired
+    OrdermAccessService ordermAccessService;
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String List(Model model) {
+        List<Orderm> listOrderm = ordermAccessService.getAllOrderm();
+        model.addAttribute("listOrderm", listOrderm);
+        model.addAttribute("nrows", listOrderm.size() + " строк(и)");
+        return "ps-dao-carwashfacility";
+    }
+}
