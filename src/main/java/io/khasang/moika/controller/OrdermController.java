8dc2d334c012759973beebc802c0b0be08d5b03d
vol2hv <vol2hv@yandex.ru>
2017-03-11 17:24:16 +0300

    промежуточный 2

--- src/main/java/io/khasang/moika/controller/OrdermController.java
+++ src/main/java/io/khasang/moika/controller/OrdermController.java
@@ -5,9 +5,11 @@ import io.khasang.moika.service.OrdermAccessService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -22,6 +24,17 @@ public class OrdermController {
         List<Orderm> listOrderm = ordermAccessService.getAllOrderm();
         model.addAttribute("listOrderm", listOrderm);
         model.addAttribute("nrows", "Количество заказов " + listOrderm.size());
+        return "orderm-list";
+    }
+
+    @RequestMapping(value = "/add", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
+    public Object addOrder(@RequestBody Orderm orderm, Model model) {
+        ordermAccessService.addOrderm(orderm);
+
+        List<Orderm> orderms = new ArrayList<>();
+        orderms.add(orderm);
+        model.addAttribute("orderms", orderms);
+        model.addAttribute("nrows", "ID: " + orderm.getId() + " Добавлен");
         return "orderm-list";
     }
 }
