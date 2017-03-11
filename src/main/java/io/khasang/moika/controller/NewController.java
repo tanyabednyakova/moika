f67eb71f2abc6c5f0f7585649ec009ba434f8381
Blajimir <blajimir@gmail.com>
2017-03-11 21:03:54 +0300

    Merge branch 'development' into akovalev
    # Conflicts:
    #	src/main/java/io/khasang/moika/config/AppConfig.java
    #	src/main/java/io/khasang/moika/controller/AKovalevController.java (reverted from commit 62bf3a54a9fe606ca024c576180d8824a8114e6e)

---
+++ src/main/java/io/khasang/moika/controller/NewController.java
@@ -0,0 +1,10 @@
+package io.khasang.moika.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping(value = "/new")
+@Controller
+public class NewController {
+
+}
